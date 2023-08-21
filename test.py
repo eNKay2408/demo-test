35a814eaf4ef201ded28dac011351841776e45f8
eNKay <142795418+eNKay2408@users.noreply.github.com>
2023-08-21 18:09:48 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-n, k = map(int, input().split())
-
-time_left = 240 - k
-time_solve = 5
-res = 0
-
-for i in range(n):
-    if time_left >= time_solve:
-        time_left -= time_solve
-        time_solve += 5
-        res += 1
-    else:
-        break
-
-print(res)
+print("Hello World")
